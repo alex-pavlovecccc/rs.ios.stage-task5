9c44cc281113f99d9a8f0a45b8d85cabd36b5d41
alex-pavlovecccc <alex.pavloveccccc@gmail.com>
2021-06-14 07:40:26 +0300

    feat: fail solution

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,26 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        var strArray: [String] = []
+                //let char = "["
+                for i in message {
+                    strArray.append(String(i))
+                }
+                
+                var value: String = ""
+                
+                for (i,j) in message.enumerated() {
+                    
+                    if j.isNumber {
+                        let ind = String(j)
+                        var valueIndex = Int(ind) ?? 0
+                    
+                        while valueIndex > 0 {
+                            value += String(strArray[i + 1])
+                            valueIndex -= 1
+                        }
+                    }
+                }
+                return value
     }
 }
